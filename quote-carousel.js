7eabe8c5a555f61efce19533e53d0159a764e5fb
Noah Shreve <122912407+nshreve@users.noreply.github.com>
2026-03-30 18:37:03 -0400

    Make slider a component and use data attr's for per-view

--- quote-carousel.js
+++ quote-carousel.js
@@ -1,15 +1,30 @@
-const swiper = new Swiper(".quotes-swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: false,
-  slidesPerView: 1.25,
-  spaceBetween: 24,
-  autoplay: {
-    delay: 5000,
-  },
-  mousewheel: {
-    enabled: true,
-    forceToAxis: true,
-    sensitivity: 50,
-  },
+const quoteComponent = document.querySelectorAll(".quotes-swiper");
+
+quoteComponent.forEach((component) => {
+  const perView = component.dataset.perView
+    ? Number(component.dataset.perView)
+    : undefined;
+  const perViewMobile = component.dataset.perViewMobile
+    ? Number(component.dataset.perViewMobile)
+    : undefined;
+
+  const swiper = new Swiper(component, {
+    direction: "horizontal",
+    loop: false,
+    slidesPerView: perViewMobile ?? 1.25,
+    spaceBetween: 24,
+    autoplay: {
+      delay: 5000,
+    },
+    mousewheel: {
+      enabled: true,
+      forceToAxis: true,
+      sensitivity: 50,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: perView ?? 1.25,
+      },
+    },
+  });
 });

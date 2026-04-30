db1d62081c7ee9cc1ef903e0225928d51ddbb9f1
Noah Shreve <122912407+nshreve@users.noreply.github.com>
2026-04-30 16:41:45 -0400

    Style thank you page if no calendly url is present

--- design-a-system/thank-you.js
+++ design-a-system/thank-you.js
@@ -1,50 +1,60 @@
-const calendlyLink = "CALENDLYLINKHERE";
+const calendlyLink = "CALENDLY LINK HERE";
 
-const HUBSPOT_PORTAL_ID = "47780539";
-const HUBSPOT_FORM_ID = "d9ce2611-9ac9-434e-ae6e-9d8d8816894c";
+const calendlyWrap = document.querySelector("[data-calendly='parent']");
+const image = document.querySelector("[data-content='image']");
+const content = document.querySelector("[data-original='paragraph-wrap']");
 
-const params = new URLSearchParams(window.location.search);
-const firstName = params.get("firstname");
-const lastName = params.get("lastname");
-const email = params.get("email");
+if (calendlyLink) {
+  const HUBSPOT_PORTAL_ID = "47780539";
+  const HUBSPOT_FORM_ID = "d9ce2611-9ac9-434e-ae6e-9d8d8816894c";
 
-Calendly.initInlineWidget({
-  url: calendlyLink,
-  parentElement: document.querySelector("[data-calendly='parent']"),
-  prefill: {
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-  },
-  utm: {
-    utmSource: "lunar-energy",
-    utmMedium: "website",
-  },
-});
+  const params = new URLSearchParams(window.location.search);
+  const firstName = params.get("firstname");
+  const lastName = params.get("lastname");
+  const email = params.get("email");
 
-window.addEventListener("message", async function (e) {
-  if (e.data.event !== "calendly.event_scheduled") return;
-  if (!email) return;
+  Calendly.initInlineWidget({
+    url: calendlyLink,
+    parentElement: calendlyWrap,
+    prefill: {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+    },
+    utm: {
+      utmSource: "lunar-energy",
+      utmMedium: "website",
+    },
+  });
 
-  try {
-    await fetch(
-      `https://api.hsforms.com/submissions/v3/integration/submit/${HUBSPOT_PORTAL_ID}/${HUBSPOT_FORM_ID}`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          fields: [
-            { name: "email", value: email },
-            { name: "has_booked_meeting", value: "true" },
-          ],
-          context: {
-            pageUri: window.location.href,
-            pageName: document.title,
-          },
-        }),
-      },
-    );
-  } catch (err) {
-    console.error("HubSpot update error:", err);
-  }
-});
+  window.addEventListener("message", async function (e) {
+    if (e.data.event !== "calendly.event_scheduled") return;
+    if (!email) return;
+
+    try {
+      await fetch(
+        `https://api.hsforms.com/submissions/v3/integration/submit/${HUBSPOT_PORTAL_ID}/${HUBSPOT_FORM_ID}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            fields: [
+              { name: "email", value: email },
+              { name: "has_booked_meeting", value: "true" },
+            ],
+            context: {
+              pageUri: window.location.href,
+              pageName: document.title,
+            },
+          }),
+        },
+      );
+    } catch (err) {
+      console.error("HubSpot update error:", err);
+    }
+  });
+} else {
+  calendlyWrap.style.height = "auto";
+  content.style.justifyContent = "center";
+  calendlyWrap.append(image);
+}
